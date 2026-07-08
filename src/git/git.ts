54effb1d122d2bf25cd130da32b0c4766b2e4429
Jenil1105 <unselfstudy@gmail.com>
2026-07-08 11:52:58 +0530

    Testing 1

--- src/git/git.ts
+++ src/git/git.ts
@@ -5,6 +5,8 @@ export function createBranch(name: string): void {
 }
 
 export function commit(message: string): void {
+    execSync('git config user.name "github-actions[bot]"');
+    execSync('git config user.email "41898282+github-actions[bot]@users.noreply.github.com"');
     execSync("git add README.md", { stdio: "inherit" });
     execSync(`git commit -m "${message}"`, { stdio: "inherit" });
 }
